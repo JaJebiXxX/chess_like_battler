efb70c7d257f33e541397e6cfbf88cfde87d747a
Ader505 <73462162+Ader505@users.noreply.github.com>
2025-10-14 17:41:06 +0200

    add frontend

---
+++ auto-chess-battler-backend/src/main/java/com/j_akacin/e_abramowicz/auto_chess_battler_backend/game/MovePattern.java
@@ -0,0 +1,5 @@
+package com.j_akacin.e_abramowicz.auto_chess_battler_backend.game;
+
+public enum MovePattern {
+    STRAIGHT, DIAGONAL, L_SHAPE
+}
---
+++ auto-chess-battler-backend/src/main/java/com/j_akacin/e_abramowicz/auto_chess_battler_backend/game/Rook.java
@@ -0,0 +1,5 @@
+package com.j_akacin.e_abramowicz.auto_chess_battler_backend.game;
+
+public class Rook {
+
+}
