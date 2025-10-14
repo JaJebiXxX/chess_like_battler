549d5979e08d8e6e8e1cf8392e162e7bb925bd73
Ader505 <73462162+Ader505@users.noreply.github.com>
2025-10-14 19:01:38 +0200

    initial commit with stuff

---
+++ backend/src/main/java/com/akcimabram/chessrbattlr/ChessrbattlrApplication.java
@@ -0,0 +1,13 @@
+package com.akcimabram.chessrbattlr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessrbattlrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessrbattlrApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/akcimabram/chessrbattlr/ChessrbattlrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akcimabram.chessrbattlr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessrbattlrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
